401079ff7a7f5e9b1d5a63b5977ff1a27d7da7e2
Jack Slide <fonlineel@yandex.ru>
2017-11-20 02:02:49 +0300

    Не корректно работает модуль записи сообщений в базу, с другой стороны а нужен ли он вообще

--- controllers/contacts.controller.php
+++ controllers/contacts.controller.php
@@ -2,8 +2,17 @@
 
 class ContactsController extends Controller{
 
-  public function index(){
+public function __construct($data = array()){
+  parent::__construct($data);
+  $this->model = new Message();
+}
 
+  public function index(){
+    if($_POST){
+      if($this->model->save($_POST)){
+        Session::setFlash('Спасибо, Ваше сообщение отправлено!');
+      }
+    }
   }
 
 }
---
+++ models/message.php
@@ -0,0 +1,35 @@
+<?php
+
+class Message extends Model{
+
+  public function save($data, $id = null){
+    if(!isset($data['name']) || !isset($data['email']) || !isset($data['message']) ) {
+      return false;
+    }
+
+    $id = (int)$id;
+    $name = $this->db->escape($data['name']);
+    $email = $this->db->escape($data['email']);
+    $message = $this->db->escape($data['message']);
+
+    if(!$id) { //добавляем новую запись
+      $sql = "
+      insert into messages
+       set name = '{$name}',
+        email = '{$email}',
+         message = '{$message}'";
+    }
+    else { //обновление записи
+      $sql = "update messages
+       set name = '{$name}',
+        email = '{$email}',
+         message = '{$message}'
+          where id = {$id}";
+    }
+
+    return $this->db->query($sql);
+  }
+}
+
+
+ ?>
